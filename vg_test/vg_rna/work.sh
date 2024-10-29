4d42d69761cfab65e90af3d783285b3faa1fd3fa
wenhai <zhangwenhai0182@outlook.com>
2024-10-29 18:57:07 +0800

    vg rna projection test

--- vg_test/vg_rna/work.sh
+++ vg_test/vg_rna/work.sh
@@ -2,6 +2,7 @@
 # GFA: two haplotypes are written as W lines
 # GTF: need to follow SAMPLE#HAPLOTYPE#LOCUS#PHASE_BLOCK(may be old path metadata model) 
 vg rna -p -n mh_chr1.gtf -n zs_chr1.gtf sub_w.gfa > spliced.gfa
+vg rna -p -a -r -e -n mh_chr1.gtf sub_w.gfa -i hap.txt > spliced.gfa
 
 # GFA: one haplotype is written as P line, which means reference. This naming must not include the # symbol. The other is written as W line.
 # GTF: The name of reference is the same as the name of P line. The name of haplotype need to follow SAMPLE#HAPLOTYPE#LOCUS#PHASE_BLOCK
